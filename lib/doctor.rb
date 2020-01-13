826c2fc05e71bf39d4c607d5a1ab84761f76810d
Eddie <mchh.004@gmail.com>
2020-01-13 15:22:03 -0500

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,33 @@
+require_relative "./appointment.rb"
+require_relative "./patient.rb"
+require 'pry'
+
+class Doctor 
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select do |appt|
+            appt.doctor == self
+        end
+    end
+
+    def new_appointment(patient,date)
+        Appointment.new(date,patient,self)
+    end
+
+    def patients
+       self.appointments.map do |appt|
+            appt.patient
+        end
+    end
+
+end
